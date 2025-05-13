3e0f5ff6281e9d6060229f755d3077cda7e8e15f
Nuel Elekwachi <elekwaeke300@gmail.com>
2025-05-13 01:21:10 +0100

    adding new models, creating signin and signout actions

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+};
+
+export default function SignInPage() {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign in</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ auth.ts
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthConfig } from "next-auth";
+
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        // Check if user exists and password matches
+        if (user && user.password) {
+          const IsMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+
+          //   If password is correct, return user
+          if (IsMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        // If usr does not exist or password does not match
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
+      session.user.id = token.sub;
+
+      //   If there is an update, set the user name
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
